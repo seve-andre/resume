ca00e182f6a4a7a82588c89fbe8b97dbecf329d9
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-03-23 15:27:24 +0100

    add current version of resume

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # curriculum-vitae
 Curriculum vitae/CV/résumé
+
+When CV is downloaded to the dir:
+- should unzip Overleaf source
+- create pdf file for both english and italian versions (.tex versions)
+- a script should detect it and deploy to github
+- github workflows should make a new release
---
+++ deploy_to_github.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+inotifywait -m . -e close_write -e moved_to |
+    while read directory action file; do
+        if [[ "$file" =~ .*zip$ ]]; then # Does the file end with .xml?
+            echo "new zip file added"
+            jar xvf resume.zip
+            echo "unzipped"
+            rm -rf resume.zip
+            xelatex *.tex
+            echo "commit message:"
+            read message
+            git add .
+            git commit -m "$message"
+            git push origin main
+        fi
+    done
