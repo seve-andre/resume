88f85c159178099c24c78ef6b7e5344898defd37
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-03-23 16:41:31 +0100

    move code out inotifywait causing bugs

--- deploy_to_github.sh
+++ deploy_to_github.sh
@@ -3,17 +3,21 @@ inotifywait -m . -e close_write -e moved_to |
     while read directory action file; do
         if [[ "$file" =~ .*zip$ ]]; then # Does the file end with .zip?
             echo "new zip file added"
-            jar xvf resume.zip
-            echo "unzipped"
-            rm -rf resume.zip
-            xelatex severi-andrea-cv-en.tex
-            echo "english to pdf done!"
-            xelatex severi-andrea-cv-it.tex
-            echo "italian to pdf done!"
-            echo "commit message:"
-            read message
-            git add .
-            git commit -m "$message"
-            git push origin main
+            break
         fi
     done
+mv cv.zip source/
+cd source/
+jar xvf cv.zip
+echo "unzipped"
+rm -rf cv.zip
+xelatex severi-andrea-cv-en.tex
+echo "english to pdf done!"
+xelatex severi-andrea-cv-it.tex
+echo "italian to pdf done!"
+mv *.pdf ../
+# echo "commit message:"
+# read message
+# git add .
+# git commit -m "$message"
+# git push origin main
